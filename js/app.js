bfb5e5997dd2d6000f8e74ef9422a5c1b4696724
Clara Mae Wells <clara.wells@selu.edu>
2017-09-25 14:06:10 -0700

    customers per hour method works

---
+++ js/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+
+var pike = {
+  location: '1st and Pike',
+  minCust: 23,
+  maxCust: 65,
+  cookiesPerCust: 6.3,
+
+  custPerHour: function() {
+    return Math.round(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
+  }
+};
