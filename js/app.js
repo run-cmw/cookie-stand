40b8b6cefab792cecb7ca5fd62099fccd01e1094
Clara Mae Wells <clara.wells@selu.edu>
2017-09-25 18:53:44 -0700

    SeaTac Airport location works as well as 1st and Pike

--- js/app.js
+++ js/app.js
@@ -49,3 +49,53 @@ var pike = {
 pike.hourlyCookiesFun();
 pike.totalCookies();
 pike.render();
+
+
+
+var seaTac = {
+  location: 'SeaTac Airport',
+  minCust: 3,
+  maxCust: 24,
+  cookiesPerCust: 1.2,
+  hourlyCookiesArr: [],
+  dailyCookies: 0,
+
+  custPerHour: function() {
+    return Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
+  },
+  cookiesPerHour: function() {
+    return Math.ceil(this.custPerHour() * this.cookiesPerCust);
+  },
+  hourlyCookiesFun: function() {
+    for (var i = 0; i < hours.length; i++) {
+      this.hourlyCookiesArr.push(this.cookiesPerHour());
+    }
+  },
+  totalCookies: function() {
+    for (var i in this.hourlyCookiesArr) {
+      this.dailyCookies += this.hourlyCookiesArr[i];
+    }
+  },
+  render: function() {
+    for (var i in this.hourlyCookiesArr) {
+      // create a new HTML element
+      var liEl = document.createElement('li');
+      // give that element content
+      liEl.textContent = hours[i] + ': ' + this.hourlyCookiesArr[i] + ' cookies';
+      // append that element to the right spot in the document
+      var seaTacUl = document.getElementById('seaTac');
+      seaTacUl.appendChild(liEl);
+    }
+    // create a new HTML element
+    var liEl2 = document.createElement('li');
+    // give that element content
+    liEl2.textContent = 'Total: ' + this.dailyCookies + ' cookies';
+    // append that element to the right spot in the document
+    var seaTacUl2 = document.getElementById('seaTac');
+    seaTacUl2.appendChild(liEl2);
+  }
+};
+
+seaTac.hourlyCookiesFun();
+seaTac.totalCookies();
+seaTac.render();
